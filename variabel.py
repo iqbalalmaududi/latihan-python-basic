5de21b5e3043b4cbc07da5af29a50641ec254627
iqbalalmaududi <115415097+iqbalalmaududi@users.noreply.github.com>
2022-10-09 22:35:08 +0700

    Add files via upload

---
+++ variabel.py
@@ -0,0 +1,8 @@
+cuaca = input("Masukkan cuaca: ")
+cuaca2 = input("masuk")
+if cuaca == "hujan":
+    print("sediakan jas hujan")
+elif cuaca == "cerah":
+    print("gunakan kacamata hitam")
+else :
+  print("input salah")  
