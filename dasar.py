87fccfff4dbce85aa0ddb61168ef818577db2a97
iqbalalmaududi <115415097+iqbalalmaududi@users.noreply.github.com>
2022-10-09 22:33:58 +0700

    Add files via upload

---
+++ dasar.py
@@ -0,0 +1,6 @@
+kacang = int(input("masukkan jumlah kacang: "))
+ciki = int(input("masukkan jumlah ciki: "))
+harga_kacang = 3000
+harga_ciki = 2000
+total = (kacang * harga_kacang) + (ciki*harga_ciki)
+print(str(total))
